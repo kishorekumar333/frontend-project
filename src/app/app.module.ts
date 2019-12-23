d6edea45850179e4a2c2cf50bd8e4fa089275c7f
kishorekumar333 <katlakishore.kumar003@gmail.com>
2019-12-23 13:27:37 +0530

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './jobseeker/dashboard/dashboard/dashboard.component';
-
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    DashboardComponent
+    AppComponent
+
   ],
   imports: [
     BrowserModule,
